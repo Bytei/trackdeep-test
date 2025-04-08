a95faab141ade80492a2bb4d1dbf5910673c2806
hindrekhannus_artify <hindrek.hannus@artify.ee>
2025-04-08 14:49:06 +0300

    Commit

--- src/components/ObjectInfo.vue
+++ src/components/ObjectInfo.vue
@@ -14,7 +14,15 @@ const props = defineProps({
 const location = computed(() => {
     const position = props.flyingObject.getCurrentPosition()
 
-    return `${position[0]} lat ${position[1]} lon}`
+    return `${position[0]} lat ${position[1]} lon`
+})
+
+const lat = computed(() => {
+    return props.flyingObject.getCurrentPosition()[0]
+})
+
+const lon = computed(() => {
+    return props.flyingObject.getCurrentPosition()[1]
 })
 
 const timeToExpire = computed(() => {
@@ -37,7 +45,13 @@ const speed = computed(() => {
         class="object-info"
     >
         <div>Speed: {{ speed }} km/h</div>
-        <div>Location: {{ location }}</div>
+        <div>Location:</div>
+        <div class="ml-2">
+            Lat: {{ lat }}
+        </div>
+        <div class="ml-2">
+            Lon: {{ lon }}
+        </div>
         <div>Time to expire: {{ timeToExpire }}</div>
         <div></div>
     </div>
@@ -51,7 +65,7 @@ const speed = computed(() => {
     z-index: 10000;
     background: rgba(0, 0, 0, 0.3);
     color: white;
-    min-width: 500px;
+    min-width: 400px;
     padding: 12px 20px;
     margin: 8px;
     border-radius: 10px;
